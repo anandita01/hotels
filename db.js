6bcb5c5b2334684495abcbcc4f85e23c12911db5
anandita01 <anandita0406@gmail.com>
2024-05-28 20:53:05 +0530

    shifte data base server to online

--- db.js
+++ db.js
@@ -1,6 +1,10 @@
 const mongoose=require('mongoose');
-const mongoURL= 'mongodb://localhost:27017/resto'
+require('dotenv').config();
 
+//const mongoURL= process.env.MONGODB_LOCAL_URL;
+//const mongoURL='mongodb+srv://anandita01:hotel123@cluster0.h2nrosp.mongodb.net/'
+
+const mongoURL=process.env.MONGODB_URL;
 mongoose.connect(mongoURL, {
     // useNewUrlParser: true,
     // useUnifiedTopology: true,
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const db = require('./db');
-
+require('dotenv').config();
 app.use(express.json());
 //body parser 
 const bodyParser= require('body-parser');
@@ -27,8 +27,8 @@ const menuItemRoutes=require('./routes/menuItemRoutes');
 app.use('/person',personRoutes);
 app.use('/menu',menuItemRoutes);
 
+const PORT=process.env.PORT || 3000;
 
-
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
   console.log('listening on port 3000')
 })
